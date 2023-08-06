356b0a9919f707457abf2ca643e3826d8fe4c153
a.yamaev@smartengines.com <rewin1996@gmail.com>
2023-08-06 05:14:29 +0300

    load missed files

--- src/prefab/load.rs
+++ src/prefab/load.rs
@@ -31,7 +31,11 @@ pub struct PrefabAutoChild;
 impl Plugin for LoadPlugin {
     fn build(&self, app: &mut App) {
         app.editor_registry::<PrefabLoader>();
-        app.add_systems(Update, load_prefab);
+        app.add_systems(Update, load_prefab
+            .after(bevy_scene_hook::Systems::SceneHookRunner));
+        app.add_systems(Update, conflict_resolve
+            .after(bevy_scene_hook::Systems::SceneHookRunner)
+            .before(load_prefab));
 
     }
 }
@@ -47,19 +51,50 @@ pub struct PrefabLoader {
 
 fn load_prefab(
     mut commands : Commands,
-    query : Query<(Entity, &PrefabLoader), Changed<PrefabLoader>>,
+    query : Query<(Entity, &PrefabLoader, Option<&Children>, Option<&Transform>, Option<&Visibility>), Changed<PrefabLoader>>,
+    auto_childs : Query<Entity, With<PrefabAutoChild>>,
     mut assets : ResMut<AssetServer>
 ) {
-    for (e, l) in query.iter() {
+    for (e, l, children, tr, vis) in query.iter() {
+
+        if tr.is_none() {
+            commands.entity(e).insert(TransformBundle::default());
+        }
+        if vis.is_none() {
+            commands.entity(e).insert(VisibilityBundle::default());
+        }
+
+        //remove old scene
+        if let Some(children) = children {
+            for child in children {
+                if auto_childs.contains(*child) {
+                    commands.entity(*child).despawn_recursive();
+                }
+            }
+            commands.entity(e).clear_children();
+        }
+
         let scene : Handle<DynamicScene> = assets.load(&l.path);
-        commands.entity(e).insert(scene)
+        commands.entity(e).with_children(|cmds| {
+            cmds.spawn(
+                DynamicSceneBundle {
+                    scene : scene,
+                    ..default()
+                }
+            )
             .insert(SceneHook::new(|e, cmd| {
-                if e.contains::<Transform>() {
-                    cmd.insert(Visibility::default());
-                }
-                if e.contains::<PrefabMarker>() {
-                    cmd.remove::<PrefabMarker>();
-                }
-            }));
+                cmd.insert(PrefabAutoChild);
+            }))
+            .insert(PrefabAutoChild);
+        });
+    }
+}
+
+fn conflict_resolve(
+    mut commands : Commands,
+    query : Query<Entity, (With<PrefabAutoChild>, With<PrefabMarker>)>
+) {
+    for e in query.iter() {
+        commands.entity(e).remove::<PrefabMarker>();
     }
 }
