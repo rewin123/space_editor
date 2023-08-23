74a002da42e31bdd46fdfc85f3728fa532a7871d
a.yamaev <a.yamaev@smartengines.com>
2023-08-23 11:46:34 +0300

    cooperative move, rotation with single gizmo

--- src/editor/inspector/mod.rs
+++ src/editor/inspector/mod.rs
@@ -271,88 +271,121 @@ pub fn inspect(
 
             let view_matrix = Mat4::from(cam_pos.affine().inverse());
 
-            for e in &selected {
-                let Some(ecell) = cell.get_entity(*e) else {
-                    continue;
-                };
-                let Some(mut transform) = ecell.get_mut::<Transform>() else {
-                    continue;
-                };
-                if let Some(parent) = ecell.get::<Parent>() {
-                    if let Some(parent) = cell.get_entity(parent.get()) {
-                        if let Some(parent_global) = parent.get::<GlobalTransform>() {
-                            if let Some(global) = ecell.get::<GlobalTransform>() {
-                                if let Some(result) = egui_gizmo::Gizmo::new(format!("Selected gizmo {:?}", *e))
-                                    .projection_matrix(cam_proj.get_projection_matrix().to_cols_array_2d())
-                                    .view_matrix(view_matrix.to_cols_array_2d())
-                                    .model_matrix(global.compute_matrix().to_cols_array_2d())
-                                    .mode(state.gizmo_mode.clone())
-                                    .interact(ui) {
-                                    
-                                    let new_transform = Transform {
-                                        translation: Vec3::from(<[f32; 3]>::from(result.translation)),
-                                        rotation: Quat::from_array(<[f32; 4]>::from(result.rotation)),
-                                        scale: Vec3::from(<[f32; 3]>::from(result.scale)),
-                                    };
-                                    
-                                    if ui.input(|state| state.modifiers.shift) {
-                                        info!("Cooperative shift with");
-                                        let dpos = new_transform.translation - transform.translation;
-                                        for e2 in &selected {
-                                            if *e != *e2 {
-                                                 if let Some(ecell) = cell.get_entity(*e2) {
-                                                    if let Some(mut transform2) = ecell.get_mut::<Transform>() {
-                                                        transform2.translation += dpos;
-                                                        transform2.set_changed();
-                                                    }
-                                                 }
-                                            }
-                                        }
-                                    }
-
-                                    let new_transform = GlobalTransform::from(new_transform);
-                                    *transform = new_transform.reparented_to(&parent_global);
-                                    transform.set_changed();
-
-                                    
-                                }
-                                disable_pan_orbit = true;
-                                continue;
-                            }
-                        }
-                    }
+            if ui.input(|s| s.modifiers.shift) && state.gizmo_mode != GizmoMode::Scale {
+                let mut mean_transform = Transform::IDENTITY;
+                for e in &selected {
+                    let Some(ecell) = cell.get_entity(*e) else {
+                        continue;
+                    };
+                    let Some(mut global_transform) = ecell.get_mut::<GlobalTransform>() else {
+                        continue;
+                    };
+                    let tr = global_transform.compute_transform();
+                    mean_transform.translation += tr.translation;
                 }
-                if let Some(result) = egui_gizmo::Gizmo::new(format!("Selected gizmo {:?}", *e))
+                mean_transform.translation /= selected.len() as f32;
+
+                let mut global_mean = GlobalTransform::from(mean_transform);
+
+                let mut loc_transform = vec![];
+                for e in &selected {
+                    let Some(ecell) = cell.get_entity(*e) else {
+                        continue;
+                    };
+                    let Some(mut global_transform) = ecell.get_mut::<GlobalTransform>() else {
+                        continue;
+                    };
+                    loc_transform.push(global_transform.reparented_to(&global_mean));
+                }
+
+                if let Some(result) = egui_gizmo::Gizmo::new(format!("Selected gizmo mean global"))
                     .projection_matrix(cam_proj.get_projection_matrix().to_cols_array_2d())
                     .view_matrix(view_matrix.to_cols_array_2d())
-                    .model_matrix(transform.compute_matrix().to_cols_array_2d())
+                    .model_matrix(mean_transform.compute_matrix().to_cols_array_2d())
                     .mode(state.gizmo_mode.clone())
                     .interact(ui) {
 
-                    if ui.input(|state| state.modifiers.shift) {
-                        info!("Cooperative shift with");
-                        let dpos = result.translation - transform.translation;
-                        for e2 in &selected {
-                            if *e != *e2 {
-                                    if let Some(ecell) = cell.get_entity(*e2) {
-                                    if let Some(mut transform2) = ecell.get_mut::<Transform>() {
-                                        transform2.translation += dpos;
-                                        transform2.set_changed();
-                                    }
-                                    }
-                            }
-                        }
-                    }
-
-                    *transform = Transform {
+                    mean_transform = Transform {
                         translation: Vec3::from(<[f32; 3]>::from(result.translation)),
                         rotation: Quat::from_array(<[f32; 4]>::from(result.rotation)),
                         scale: Vec3::from(<[f32; 3]>::from(result.scale)),
                     };
-                    transform.set_changed();
                     disable_pan_orbit = true;
                 }
-                
+
+                global_mean = GlobalTransform::from(mean_transform);
+
+                for (idx, e) in selected.iter().enumerate() {
+                    let Some(ecell) = cell.get_entity(*e) else {
+                        continue;
+                    };
+                    let Some(mut transform) = ecell.get_mut::<Transform>() else {
+                        continue;
+                    };
+
+                    let new_global = global_mean.mul_transform(loc_transform[idx]);
+
+                    if let Some(parent) = ecell.get::<Parent>() {
+                        if let Some(parent) = cell.get_entity(parent.get()) {
+                            if let Some(parent_global) = parent.get::<GlobalTransform>() {
+                                *transform = new_global.reparented_to(&parent_global);
+                            }
+                        }
+                    } else {
+                        *transform = new_global.compute_transform();
+                    }
+                }
+            } else {
+                for e in &selected {
+                    let Some(ecell) = cell.get_entity(*e) else {
+                        continue;
+                    };
+                    let Some(mut transform) = ecell.get_mut::<Transform>() else {
+                        continue;
+                    };
+                    if let Some(parent) = ecell.get::<Parent>() {
+                        if let Some(parent) = cell.get_entity(parent.get()) {
+                            if let Some(parent_global) = parent.get::<GlobalTransform>() {
+                                if let Some(global) = ecell.get::<GlobalTransform>() {
+                                    if let Some(result) = egui_gizmo::Gizmo::new(format!("Selected gizmo {:?}", *e))
+                                        .projection_matrix(cam_proj.get_projection_matrix().to_cols_array_2d())
+                                        .view_matrix(view_matrix.to_cols_array_2d())
+                                        .model_matrix(global.compute_matrix().to_cols_array_2d())
+                                        .mode(state.gizmo_mode.clone())
+                                        .interact(ui) {
+                                        
+                                        let new_transform = Transform {
+                                            translation: Vec3::from(<[f32; 3]>::from(result.translation)),
+                                            rotation: Quat::from_array(<[f32; 4]>::from(result.rotation)),
+                                            scale: Vec3::from(<[f32; 3]>::from(result.scale)),
+                                        };
+
+                                        let new_transform = GlobalTransform::from(new_transform);
+                                        *transform = new_transform.reparented_to(&parent_global);
+                                        transform.set_changed();
+                                    }
+                                    disable_pan_orbit = true;
+                                    continue;
+                                }
+                            }
+                        }
+                    }
+                    if let Some(result) = egui_gizmo::Gizmo::new(format!("Selected gizmo {:?}", *e))
+                        .projection_matrix(cam_proj.get_projection_matrix().to_cols_array_2d())
+                        .view_matrix(view_matrix.to_cols_array_2d())
+                        .model_matrix(transform.compute_matrix().to_cols_array_2d())
+                        .mode(state.gizmo_mode.clone())
+                        .interact(ui) {
+
+                        *transform = Transform {
+                            translation: Vec3::from(<[f32; 3]>::from(result.translation)),
+                            rotation: Quat::from_array(<[f32; 4]>::from(result.rotation)),
+                            scale: Vec3::from(<[f32; 3]>::from(result.scale)),
+                        };
+                        transform.set_changed();
+                        disable_pan_orbit = true;
+                    }
+                }
             }
         });
 
