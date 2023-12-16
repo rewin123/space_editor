7c348bd8bc2dede20d82767abc65e0b0ce13e768
rewin <rewin1996@gmail.com>
2023-12-16 12:15:51 +0300

    Update space_example.rs

--- examples/space_example.rs
+++ examples/space_example.rs
@@ -3,7 +3,7 @@ use space_editor::prelude::*;
 
 fn main() {
     App::default()
-        .add_plugins((DefaultPlugins, EditorPlugin))
+        .add_plugins(DefaultPlugins)
         .add_plugins(SpaceEditorPlugin)
         .add_systems(Startup, simple_editor_setup)
         .run();
