fd9c84a7d3a439673f0f1f4fda2bf2ac43080125
a.yamaev <a.yamaev@smartengines.com>
2023-12-21 15:23:23 +0300

    update authors

--- publish.bat
+++ publish.bat
@@ -1,1 +1,32 @@
+cd crates/shared
+cargo publish --dry-run
 timeout /t 10
+cd ..
+cd undo
+cargo publish --dry-run
+timeout /t 10
+cd ..
+cd prefab
+cargo publish --dry-run
+timeout /t 10
+cd ..
+cd persistance
+cargo publish --dry-run
+timeout /t 10
+cd ..
+cd editor_core
+cargo publish
+timeout /t 10
+cd ..
+cd editor_ui
+cargo publish
+timeout /t 10
+cd ..
+cd ..
+cd modules
+cd bevy_xpbd_plugin
+cargo publish
+timeout /t 10
+cd ..
+cd ..
+cargo publish
