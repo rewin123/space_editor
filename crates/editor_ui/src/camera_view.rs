bc37b3d40cf3127b8c3aac50c116e124b541b56f
Mick Harrigan <mharrigan328@gmail.com>
2024-01-03 19:42:15 -0800

    Small change to remove useless cycles, will need more work later

--- crates/editor_ui/src/camera_view.rs
+++ crates/editor_ui/src/camera_view.rs
@@ -78,32 +78,32 @@ fn create_camera_image(width: u32, height: u32) -> Image {
 
 impl EditorTab for CameraViewTab {
     fn ui(&mut self, ui: &mut bevy_egui::egui::Ui, commands: &mut Commands, world: &mut World) {
-        if self.real_camera.is_none() {
-            self.real_camera = Some(
-                commands
-                    .spawn((
-                        Camera3dBundle {
-                            camera: Camera {
-                                is_active: false,
-                                order: 2,
-                                ..default()
-                            },
-                            camera_3d: Camera3d {
-                                clear_color:
-                                    bevy::core_pipeline::clear_color::ClearColorConfig::Default,
-                                ..default()
-                            },
-                            ..default()
-                        },
-                        RenderLayers::layer(0),
-                        TemporalJitter::default(),
-                        Name::new("Camera for Camera view tab"),
-                        DisableCameraSkip,
-                        ViewCamera,
-                    ))
-                    .id(),
-            );
-        }
+        // if self.real_camera.is_none() {
+        //     self.real_camera = Some(
+        //         commands
+        //             .spawn((
+        //                 Camera3dBundle {
+        //                     camera: Camera {
+        //                         is_active: false,
+        //                         order: 2,
+        //                         ..default()
+        //                     },
+        //                     camera_3d: Camera3d {
+        //                         clear_color:
+        //                             bevy::core_pipeline::clear_color::ClearColorConfig::None,
+        //                         ..default()
+        //                     },
+        //                     ..default()
+        //                 },
+        //                 // RenderLayers::layer(0),
+        //                 TemporalJitter::default(),
+        //                 Name::new("Camera for Camera view tab"),
+        //                 DisableCameraSkip,
+        //                 ViewCamera,
+        //             ))
+        //             .id(),
+        //     );
+        // }
 
         let mut camera_query = world.query_filtered::<Entity, (
             With<Camera>,
--- crates/editor_ui/src/meshless_visualizer.rs
+++ crates/editor_ui/src/meshless_visualizer.rs
@@ -1,4 +1,8 @@
-use bevy::{prelude::*, render::view::RenderLayers};
+use bevy::{
+    prelude::*,
+    render::view::RenderLayers,
+    sprite::{Material2d, MaterialMesh2dBundle, Mesh2d, Mesh2dHandle},
+};
 use space_shared::*;
 
 #[derive(Default)]
@@ -12,8 +16,21 @@ impl Plugin for MeshlessVisualizerPlugin {
     }
 }
 
+/// Marks the applied entity as needing to be visualized. This is added via users to make any custom entities have a mesh that they can control
+/// also comes with the visual that will be used for the object in question.
+// #[derive(Bundle)]
 #[derive(Component)]
-pub struct EditorViewOnly;
+pub struct CustomMeshless {
+    // /// Visual that will be used to show the entity or object
+    pub visual: MatMesh,
+}
+
+pub struct MatMesh {
+    // TODO: figure out what to put with the material so that the compiler doesn't cry
+    // material: Box<dyn Material>,
+    material: StandardMaterial,
+    mesh: Mesh,
+}
 
 #[derive(Resource, Default)]
 pub struct MeshlessMeshMat {
@@ -23,35 +40,72 @@ pub struct MeshlessMeshMat {
 
 pub fn visualize_meshless(
     mut commands: Commands,
-    cams: Query<(Entity, &Transform), (With<Camera>, Without<EditorCameraMarker>)>,
-    lights: Query<
-        (Entity, &Transform),
-        Or<(With<DirectionalLight>, With<SpotLight>, With<PointLight>)>,
+    objects: Query<
+        (
+            Entity,
+            &Transform,
+            Option<&Handle<Mesh>>,
+            Option<&Handle<StandardMaterial>>,
+        ),
+        Or<(
+            (With<Camera>, Without<EditorCameraMarker>),
+            Or<(With<DirectionalLight>, With<SpotLight>, With<PointLight>)>,
+        )>,
     >,
     handle: Res<MeshlessMeshMat>,
 ) {
-    for (entity, transform) in &cams {
-        commands
-            .entity(entity)
-            .insert(MaterialMeshBundle {
-                mesh: handle.mesh.clone(),
-                material: handle.mat.clone(),
-                transform: *transform,
-                ..default()
-            })
-            .insert(RenderLayers::layer((RenderLayers::TOTAL_LAYERS - 1) as u8));
+    for (entity, transform, mesh, mat) in &objects {
+        match (mesh, mat) {
+            (Some(_), Some(_)) => {}
+            _ => {
+                commands.entity(entity).insert((
+                    MaterialMeshBundle {
+                        mesh: handle.mesh.clone(),
+                        material: handle.mat.clone(),
+                        transform: *transform,
+                        ..default()
+                    },
+                    RenderLayers::layer((RenderLayers::TOTAL_LAYERS - 1) as u8),
+                ));
+            }
+        }
     }
+}
 
-    for (entity, transform) in &lights {
-        commands
-            .entity(entity)
-            .insert(MaterialMeshBundle {
-                mesh: handle.mesh.clone(),
-                material: handle.mat.clone(),
-                transform: *transform,
-                ..default()
-            })
-            .insert(RenderLayers::layer((RenderLayers::TOTAL_LAYERS - 1) as u8));
+/// This will create a way to have any entity with CustomMeshlessMarker have a way to be visualized by the user
+/// Additionally, the user can either choose their own mesh and material to use or default to the white sphere
+pub fn visualize_custom_meshless(
+    mut commands: Commands,
+    ass: AssetServer,
+    objects: Query<(
+        Entity,
+        &Transform,
+        &CustomMeshless,
+        Option<&Handle<Mesh>>,
+        Option<&Handle<StandardMaterial>>,
+    )>,
+) {
+    /* NOTES: Maybe this should instead of a struct that is a component, there should be a trait that
+            can be impl'd such that the user can pair up different Components and meshes that this function then handles overall.
+            An example could be that `objects` is (Entity, &Transform) With<impl CustomMeshless> so then anything that impls it by the user can
+            be visualized via their impl, otherwise should be defaulted (or derived default if need be).
+    */
+
+    for (entity, transform, custom, mesh, mat) in objects.iter() {
+        match (mesh, mat) {
+            (Some(_), Some(_)) => {}
+            _ => {
+                commands.entity(entity).insert((
+                    MaterialMeshBundle {
+                        mesh: ass.add(custom.visual.mesh.clone()),
+                        material: ass.add(custom.visual.material.clone()),
+                        transform: *transform,
+                        ..default()
+                    },
+                    RenderLayers::layer((RenderLayers::TOTAL_LAYERS - 1) as u8),
+                ));
+            }
+        }
     }
 }
 
@@ -68,5 +122,8 @@ pub fn setup_meshless_mesh(
         }
         .into(),
     );
-    handle.mat = mats.add(StandardMaterial::default());
+    handle.mat = mats.add(StandardMaterial {
+        unlit: true,
+        ..default()
+    });
 }
