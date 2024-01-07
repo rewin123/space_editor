fc9aaf5a0b3453cad1a6ccbc4a362d5c09a32605
ncomendant <nicholas.comendant@gmail.com>
2024-01-07 06:42:30 -0800

    Add GamepadEvent

--- crates/prefab/src/events.rs
+++ crates/prefab/src/events.rs
@@ -1,4 +1,5 @@
-use bevy::{prelude::*, reflect::GetTypeRegistration};
+use crate::prelude::EditorRegistryBevyExt;
+use bevy::{input::gamepad::*, prelude::*, reflect::GetTypeRegistration};
 use std::any::TypeId;
 
 pub(crate) trait BevyEvent:
@@ -17,7 +18,36 @@ pub(crate) trait BevyEvent:
     }
 }
 
+#[derive(Event, Resource, Reflect, Clone)]
+#[reflect(Default)]
+pub(crate) struct WrappedGamepadEvent(GamepadEvent);
+
+impl Default for WrappedGamepadEvent {
+    fn default() -> Self {
+        Self(GamepadEvent::Connection(GamepadConnectionEvent {
+            gamepad: Gamepad::new(0),
+            connection: GamepadConnection::Disconnected,
+        }))
+    }
+}
+
+impl BevyEvent for WrappedGamepadEvent {
+    type T = GamepadEvent;
+
+    fn inner_event(&self) -> &Self::T {
+        &self.0
+    }
+}
+
 pub(crate) fn register_events(app: &mut App) -> &mut App {
+    app.editor_registry_bevy_event::<WrappedGamepadEvent>()
+        .register_type::<WrappedGamepadEvent>()
+        .register_type::<GamepadEvent>()
+        .register_type::<GamepadConnection>()
+        .register_type::<GamepadConnectionEvent>()
+        .register_type::<GamepadButtonChangedEvent>()
+        .register_type::<GamepadAxisChangedEvent>()
+        .register_type::<GamepadInfo>()
+
     // TODO: add all events and relevant types (https://github.com/rewin123/space_editor/issues/159)
-    app
 }
