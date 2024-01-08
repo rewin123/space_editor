1bc65fe544dd562eca70f8aaefaae8080f467f44
Julia Naomi <jnboeira@outlook.com>
2024-01-07 19:18:46 -0600

    fix

--- modules/terrain_gen/src/lib.rs
+++ modules/terrain_gen/src/lib.rs
@@ -67,51 +67,5 @@ pub fn update_spawned_terrain(
 ) {
     for entity in query.iter_mut() {
         update_events.send(UpdateTerrain::One(entity));
-        app.register_type::<MapSettings>()
-            .register_type::<HeightMap>()
-            .register_type::<SmoothFunction>()
-            .editor_registry::<TerrainDrawTag>();
-        app.add_event::<UpdateTerrain>();
-        app.editor_bundle(
-            TERRAIN_BUNDLE_CATEGORY,
-            "Square terrain",
-            TerrainBundle::default(),
-        );
-        app.add_systems(PostUpdate, update_spawned_terrain);
-    }
-}
-
-#[derive(Event, Clone)]
-pub enum UpdateTerrain {
-    All,
-    One(Entity),
-}
-
-#[derive(Bundle, Default, Clone)]
-pub struct TerrainBundle {
-    pub terrain: Terrain,
-    pub settings: MapSettings,
-    pub heightmap: HeightMap,
-    pub transform: Transform,
-    pub global_transform: GlobalTransform,
-    pub visibility: Visibility,
-    pub computed_visibility: InheritedVisibility,
-    pub view_visibility: ViewVisibility,
-    pub prefab_marker: PrefabMarker,
-}
-
-#[derive(Component, Default, Clone)]
-pub struct Terrain;
-
-#[derive(Component)]
-pub struct TerrainChunk;
-
-//Draw spawned terrain
-pub fn update_spawned_terrain(
-    mut update_events: EventWriter<UpdateTerrain>,
-    mut query: Query<Entity, Added<Terrain>>,
-) {
-    for entity in query.iter_mut() {
-        update_events.send(UpdateTerrain::One(entity));
     }
 }
