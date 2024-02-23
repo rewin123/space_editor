82f9e995f4f4e0adc48030a6d78cb235063facb2
Julia Naomi <jnboeira@outlook.com>
2024-02-22 18:03:17 -0600

    add-filter-bar

--- crates/editor_ui/src/inspector/mod.rs
+++ crates/editor_ui/src/inspector/mod.rs
@@ -213,64 +213,73 @@ pub fn inspect(ui: &mut egui::Ui, world: &mut World, open_components: &mut HashM
                 name = format!("{:?}", e.id());
             }
             ui.heading(&name);
-            ui.label("Components:");
+            let mut state = unsafe { cell.get_resource_mut::<FilterComponentState>().unwrap() };
+            ui.text_edit_singleline(&mut state.component_add_filter);
+            let lower_filter = state.component_add_filter.to_lowercase();
             let e_id = e.id().index();
+            ui.label("Components:");
             egui::Grid::new(format!("{e_id}")).show(ui, |ui| {
                 for (c_id, t_id, name, _) in &components_id {
-                    if let Some(data) = unsafe { e.get_mut_by_id(*c_id) } {
-                        let registration = registry.get(*t_id).unwrap();
-                        if let Some(reflect_from_ptr) = registration.data::<ReflectFromPtr>() {
-                            let (ptr, mut set_changed) = mut_untyped_split(data);
+                    if name.to_lowercase().contains(&lower_filter) {
+                        if let Some(data) = unsafe { e.get_mut_by_id(*c_id) } {
+                            let registration = registry.get(*t_id).unwrap();
+                            if let Some(reflect_from_ptr) = registration.data::<ReflectFromPtr>() {
+                                let (ptr, mut set_changed) = mut_untyped_split(data);
 
-                            let value = unsafe { reflect_from_ptr.from_ptr_mut()(ptr) };
+                                let value = unsafe { reflect_from_ptr.from_ptr_mut()(ptr) };
 
-                            if !editor_registry.silent.contains(&registration.type_id()) {
-                                ui.push_id(format!("{:?}-{}", &e.id(), &name), |ui| {
-                                    let header = egui::CollapsingHeader::new(name)
-                                        .default_open(*open_components.get(name).unwrap_or(&false))
-                                        .show(ui, |ui| {
-                                            ui.push_id(
-                                                format!("content-{:?}-{}", &e.id(), &name),
+                                if !editor_registry.silent.contains(&registration.type_id()) {
+                                    ui.push_id(format!("{:?}-{}", &e.id(), &name), |ui| {
+                                        let header = egui::CollapsingHeader::new(name)
+                                            .default_open(
+                                                *open_components.get(name).unwrap_or(&false),
+                                            )
+                                            .show(ui, |ui| {
+                                                ui.push_id(
+                                                    format!("content-{:?}-{}", &e.id(), &name),
+                                                    |ui| {
+                                                        if env.ui_for_reflect_with_options(
+                                                            value,
+                                                            ui,
+                                                            ui.id(),
+                                                            &(),
+                                                        ) {
+                                                            set_changed();
+                                                        }
+                                                    },
+                                                );
+                                            });
+                                        if header.header_response.clicked() {
+                                            let open_name =
+                                                open_components.entry(name.clone()).or_default();
+                                            //At click header not opened simultaneously so its need to check percent of opened
+                                            *open_name = header.openness < 0.5;
+                                        }
+                                    });
+
+                                    ui.push_id(
+                                        format!("del component {:?}-{}", &e.id(), &name),
+                                        |ui| {
+                                            //must be on top
+                                            ui.with_layout(
+                                                egui::Layout::top_down(egui::Align::Min),
                                                 |ui| {
-                                                    if env.ui_for_reflect_with_options(
-                                                        value,
-                                                        ui,
-                                                        ui.id(),
-                                                        &(),
-                                                    ) {
-                                                        set_changed();
+                                                    let button = egui::Button::new("🗙")
+                                                        .fill(DEFAULT_BG_COLOR);
+                                                    if ui.add(button).clicked() {
+                                                        commands.push(
+                                                            InspectCommand::RemoveComponent(
+                                                                e.id(),
+                                                                *t_id,
+                                                            ),
+                                                        );
                                                     }
                                                 },
                                             );
-                                        });
-                                    if header.header_response.clicked() {
-                                        let open_name =
-                                            open_components.entry(name.clone()).or_default();
-                                        //At click header not opened simultaneously so its need to check percent of opened
-                                        *open_name = header.openness < 0.5;
-                                    }
-                                });
-
-                                ui.push_id(
-                                    format!("del component {:?}-{}", &e.id(), &name),
-                                    |ui| {
-                                        //must be on top
-                                        ui.with_layout(
-                                            egui::Layout::top_down(egui::Align::Min),
-                                            |ui| {
-                                                let button =
-                                                    egui::Button::new("🗙").fill(DEFAULT_BG_COLOR);
-                                                if ui.add(button).clicked() {
-                                                    commands.push(InspectCommand::RemoveComponent(
-                                                        e.id(),
-                                                        *t_id,
-                                                    ));
-                                                }
-                                            },
-                                        );
-                                    },
-                                );
-                                ui.end_row();
+                                        },
+                                    );
+                                    ui.end_row();
+                                }
                             }
                         }
                     }
