b33cd118e62bc3d501ee5f7e3c7299c1c0b8ea40
Julia Naomi <naomijub@users.noreply.github.com>
2024-02-24 19:33:13 -0600

    cleanup (#215)
    * cleanup

--- crates/editor_ui/src/selection.rs
+++ crates/editor_ui/src/selection.rs
@@ -25,11 +25,11 @@ pub fn auto_add_picking(
     query: Query<Entity, (With<PrefabMarker>, Without<Pickable>)>,
 ) {
     for e in query.iter() {
-        commands
-            .entity(e)
-            .insert(PickableBundle::default())
-            .insert(On::<Pointer<Down>>::send_event::<SelectEvent>())
-            .insert(RaycastPickable);
+        commands.entity(e).insert((
+            PickableBundle::default(),
+            On::<Pointer<Down>>::send_event::<SelectEvent>(),
+            RaycastPickable,
+        ));
     }
 }
 
@@ -45,8 +45,7 @@ pub fn auto_add_picking_dummy(
             if mesh.primitive_topology() == PrimitiveTopology::TriangleList {
                 commands
                     .entity(e)
-                    .insert(PickableBundle::default())
-                    .insert(RaycastPickable);
+                    .insert((PickableBundle::default(), RaycastPickable));
             }
         }
     }
