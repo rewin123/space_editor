b3cd432f3dfdb6c0bda2af0addbaa5bc87ddcbb2
Julia Naomi <naomijub@users.noreply.github.com>
2024-03-28 11:41:28 -0500

    crash-fix (#276)

--- crates/editor_tabs/src/tab_style.rs
+++ crates/editor_tabs/src/tab_style.rs
@@ -1,7 +1,9 @@
 use bevy::prelude::*;
-use bevy_egui::egui::{self, RichText};
+use bevy_egui::egui::{self, Color32, RichText};
 
 pub const DEFAULT_STYLE: DefaultStyle = DefaultStyle;
+pub const DEFAULT_TAB_FONT_SIZE: f32 = 12.0;
+const ERROR_COLOR: Color32 = Color32::from_rgb(255, 59, 33);
 
 /// This trait use to get tab style
 pub trait TabStyle: Resource {
@@ -24,6 +26,15 @@ pub struct CollectedStyle {
     pub text_size: f32,
 }
 
+impl Default for CollectedStyle {
+    fn default() -> Self {
+        Self {
+            error_color: ERROR_COLOR,
+            text_size: DEFAULT_TAB_FONT_SIZE,
+        }
+    }
+}
+
 pub fn to_label(text: &str, size: f32) -> RichText {
     RichText::new(text)
         .size(size)
@@ -35,12 +46,12 @@ pub struct DefaultStyle;
 
 impl TabStyle for DefaultStyle {
     fn error_color(&self) -> egui::Color32 {
-        egui::Color32::RED
+        ERROR_COLOR
     }
 
     fn set_egui_style(&self, _world: &World, _style: &mut egui::Style) {}
 
     fn text_size(&self, _world: &World) -> f32 {
-        14.0
+        DEFAULT_TAB_FONT_SIZE
     }
 }
