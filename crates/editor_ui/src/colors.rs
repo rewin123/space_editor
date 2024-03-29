23c8ddf308ad6540ca1de25409be9a6937e92d52
Julia Naomi <naomijub@users.noreply.github.com>
2024-03-29 16:25:46 -0500

    warns-if-compiled-on-debug-build (#277)
    * warns-if-compiled-on-debug-build
    * Adds windows and Macos test pipeline

--- crates/editor_ui/src/colors.rs
+++ crates/editor_ui/src/colors.rs
@@ -10,6 +10,6 @@ pub const DEFAULT_BG_COLOR: Color32 = Color32::from_rgb(27, 27, 27);
 pub const PLAY_COLOR: Color32 = Color32::from_rgb(0, 194, 149);
 pub const ERROR_COLOR: Color32 = Color32::from_rgb(255, 59, 33);
 pub const HYPERLINK_COLOR: Color32 = Color32::from_rgb(99, 235, 231);
-pub const WARM_COLOR: Color32 = Color32::from_rgb(225, 206, 67);
+pub const WARN_COLOR: Color32 = Color32::from_rgb(225, 206, 67);
 pub const SELECTED_ITEM_COLOR: Color32 = Color32::from_rgb(76, 93, 235);
 pub const TEXT_COLOR: Color32 = Color32::WHITE;
