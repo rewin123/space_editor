f6b5a833034db7b435113ca4900d2b70ce3416bc
Julia Naomi <jnboeira@outlook.com>
2024-04-05 13:05:19 -0500

    fmt

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use space_editor_ui::{game_mode_changed, settings::GameModeSettings, simple_edit
 
 fn main() {
     /// Backtrace only supported on unix
-    #[cfg(all(feature = "backtrace" , not(target_os = "windows")))]
+    #[cfg(all(feature = "backtrace", not(target_os = "windows")))]
     unsafe {
         backtrace_on_stack_overflow::enable()
     };
-    
+
     let mut app = App::new();
     app.add_plugins(DefaultPlugins.set(WindowPlugin {
         primary_window: Some(Window {
